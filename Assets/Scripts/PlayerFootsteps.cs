5eb660ce4463fd99e31a54a37c76887cc088985d
MrsFrisby <59320229+MrsFrisby@users.noreply.github.com>
2024-08-18 07:35:06 +0100

    InitialCommit

---
+++ Assets/Scripts/PlayerFootsteps.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerFootsteps : MonoBehaviour
+{
+
+    public AudioSource footstepPlayer;
+
+    //public AudioClip[] tileSteps;
+    //public AudioClip[] metalSteps;
+    public AudioClip[] streetStepsWalk;
+    public AudioClip[] streetStepsRun;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        footstepPlayer = GetComponent<AudioSource>();
+    }
+
+    private void PlayFootsteps()   
+    {
+        if (Input.anyKey)
+        //Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow)
+        {
+            int randomIndex = Random.Range(0, streetStepsWalk.Length-1);
+            AudioClip randomClip = streetStepsWalk[randomIndex];
+
+            footstepPlayer.clip = randomClip;
+            footstepPlayer.Play();
+            Debug.Log(randomClip.name);
+        }
+        
+    }
+
+    private void PlayRunningFootsteps()
+    {
+        int randomRunIndex = Random.Range(0, streetStepsRun.Length-1);
+        AudioClip randomRunClip = streetStepsRun[randomRunIndex];
+
+        footstepPlayer.clip = randomRunClip;
+        footstepPlayer.Play();
+    }
+}
