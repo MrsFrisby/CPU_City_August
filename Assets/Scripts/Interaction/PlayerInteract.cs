b65b2af40e6f75eadf56974b78e6a340f1b66fc1
Taner Paker <taner.2001@hotmail.com>
2024-08-22 06:03:04 +0300

    Added trigger doors
    Warning: In order for the doors to work RhiVyse should have the Tag: Player

--- Assets/Scripts/Interaction/PlayerInteract.cs
+++ Assets/Scripts/Interaction/PlayerInteract.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static Data.Util.KeywordDependentCollection;
+
+//using static Data.Util.KeywordDependentCollection;
 
 public class PlayerInteract : MonoBehaviour
 {
