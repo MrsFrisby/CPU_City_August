41e1166b12cafd6b73e986e801a60471e2fd557c
Taner Paker <taner.2001@hotmail.com>
2024-08-28 03:22:19 +0300

    added text to enter the car

---
+++ Assets/Scripts/Car/CarTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CarTrigger : MonoBehaviour
+{
+    public TMP_Text carText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the collider belongs to the player
+        if (other.CompareTag("Player"))
+        {
+            // Log character
+            Debug.Log("Car trigger engaged");
+
+            // Draw a text box
+            carText.text = "Press E to enter car";
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if the collider belongs to the player
+        if (other.CompareTag("Player"))
+        {
+            // Log character
+            Debug.Log("Car trigger disengaged");
+
+            // Disengage the text box
+            carText.text = "";
+        }
+    }
+}
