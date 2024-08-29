c39ca87e8e3718a3499ad2348ea5873e0400b299
Taner Paker <taner.2001@hotmail.com>
2024-08-29 07:45:19 +0300

    Updated car dynamics
    Still needs to be worked on

--- Assets/Scripts/Car/CarTrigger.cs
+++ Assets/Scripts/Car/CarTrigger.cs
@@ -6,6 +6,52 @@ using TMPro;
 public class CarTrigger : MonoBehaviour
 {
     public TMP_Text carText;
+    public MonoBehaviour CarController;
+    public Transform Car;
+    public Transform Player;
+
+    [Header("Cameras")]
+    public GameObject PlayerCam;
+    public GameObject CarCam;
+
+    bool canDrive;
+    bool driving;
+
+    void Start()
+    {
+        CarController.enabled = false;
+        PlayerCam.gameObject.SetActive(true);
+        CarCam.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F) && canDrive)
+        {
+            CarController.enabled = true;
+            driving = true;
+            Player.transform.SetParent(Car);
+            Player.gameObject.SetActive(false);
+
+            // Camera
+            PlayerCam.gameObject.SetActive(false);
+            CarCam.gameObject.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            CarController.enabled = false;
+            driving = false;
+
+            Player.transform.SetParent(null);
+            Player.gameObject.SetActive(true);
+
+            // Camera
+            PlayerCam.gameObject.SetActive(true);
+            CarCam.gameObject.SetActive(false);
+        }
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -21,6 +67,15 @@ public class CarTrigger : MonoBehaviour
         }
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        // Check if the collider belongs to the player
+        if (other.CompareTag("Player"))
+        {
+            canDrive = true;
+        }
+    }
+
     private void OnTriggerExit(Collider other)
     {
         // Check if the collider belongs to the player
@@ -31,6 +86,8 @@ public class CarTrigger : MonoBehaviour
 
             // Disengage the text box
             carText.text = "";
+
+            canDrive = false;
         }
     }
 }
